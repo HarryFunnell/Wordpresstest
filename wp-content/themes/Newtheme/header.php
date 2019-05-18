e00b227c9875133226be0de720a0c78162a6ea8a
HarryFunnell <harryfunnell@gmail.com>
2019-05-18 21:04:02 +0100

    Update 18/05/2019

--- wp-content/themes/Newtheme/header.php
+++ wp-content/themes/Newtheme/header.php
@@ -9,3 +9,9 @@
 
 
 <body <?php body_class();?>>
+
+<header class="sticky-top">
+
+<?php wp_nav_menu( array( 'theme_location' => 'max_mega_menu_1' ) ); ?>
+
+</header>
--- wp-content/themes/Newtheme/page-about-use.php
+++ wp-content/themes/Newtheme/page-about-use.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Blank Page
+Template Name: Blank2 Page
 */
 ?>
 <?php get_header();?>
@@ -10,11 +10,11 @@ Template Name: Blank Page
     if( have_posts() ):
         while( have_posts() ): the_post(); 
         
-        the_content();
     endwhile; else:
     ?>
     <?php endif; ?>
-
-    
+    <h1 class="entry-title"> <?php the_title(); ?> </h1>
+    <?php the_content(); ?> 
+    <h3> HELLO HARRY </h3>
 </div>
 <?php get_footer();?>
